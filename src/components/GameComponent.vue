adf6b6925a01531eb79e01ddd455df14f8f46c43
Jonteton <jonteton@gmail.com>
2020-04-16 01:00:48 +0200

    hej hora

---
+++ src/components/GameComponent.vue
@@ -0,0 +1,44 @@
+
+
+<template>
+  <div>
+    <canvas ref="game_canvas" width="700" height = "512"></canvas>
+    <h1> tjo </h1>
+  </div>
+</template>
+
+<script>
+
+import game_background from "../assets/game_background.png";
+
+export default {
+  name: "Game",
+  props: {
+    msg: String
+  },
+
+
+  methods: {
+    draw () {
+      let canvas = this.$refs.game_canvas;
+      let ctx = canvas.getContext('2d');
+      let bg = new Image();
+      bg.src = game_background;
+      bg.onload = function () {
+        ctx.drawImage(bg, 0, 0)
+      }
+      ctx.fillStyle = "blue";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+  },
+
+  mounted: function () {
+    this.draw()
+  },
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/Game.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Game">
+    <h1>Hej och välkommen till vårt spel, hora</h1>
+    <GameComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GameComponent from "@/components/GameComponent.vue";
+
+export default {
+  name: "Home",
+  components: {
+    GameComponent
+  }
+};
+</script>
+
