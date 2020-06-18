6a3d11f3d9d2addf0505a62ff6271a6dc044a356
Jonteton <jonteton@gmail.com>
2020-06-18 17:53:04 +0200

    ny patch

---
+++ src/assets/lang/eng.js
@@ -0,0 +1,10 @@
+let eng_lang = {
+    "start" : "start game",
+    "langswitch" : "Change to swedish",
+    "langtext" : "Choose language below",
+    "scoretext" : "Your score is: ",
+    "tutorial" : "Press Space to dive and increase your speed!"
+}
+
+export {eng_lang}
+
---
+++ src/assets/lang/swe.js
@@ -0,0 +1,11 @@
+let swe_lang = {
+    "start" : "starta spelet",
+    "langswitch" : "byt språk till engelska",
+    "langtext" : "Välj önskat språk nedan",
+    "scoretext" : "Din poäng ",
+    "tutorial" : "Tryck mellanslag för att dyka och öka din fart!"
+
+
+}
+
+export {swe_lang}
